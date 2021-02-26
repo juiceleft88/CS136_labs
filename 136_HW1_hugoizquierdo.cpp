fd9e1ba0d568327d8c7b9a2dab9cf674a2d064f3
juiceleft88 <juiceleft88@gmail.com>
2021-02-26 07:42:22 -0800

    beginnning framework

---
+++ 136_HW1_hugoizquierdo.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <iomanip>
+#include "136_HW1_hugoizquierdo.h"
+
+using namespace std;
---
+++ 136_HW1_hugoizquierdo.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class romanType
+{
+    public:
+    void printRom (romNum[], int sizeRom)
+    void printDec (decNum[], int sizeDec)
+}
