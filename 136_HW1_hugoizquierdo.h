fc99794b80dc3c18b61926056375b7b3669362b0
juiceleft88 <juiceleft88@gmail.com>
2021-02-26 10:20:10 -0800

    functions

--- 136_HW1_hugoizquierdo.h
+++ 136_HW1_hugoizquierdo.h
@@ -1,10 +1,14 @@
-/*
 #pragma once
 
 class romanType
 {
     public:
-    void printRom (romNum[], int sizeRom);
-    void printDec (decNum[], int sizeDec);
+    void printRom (romNum[SIZE]);
+    int printDec (decNum[SIZE]);
+    int convertRom (int sum);
+
+    private:
+    const int SIZE = 7;
+    char romNum[SIZE] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    int sum;
 };
-*/
