a6afb682821e8b04637533daced37a32dbb217f3
juiceleft88 <juiceleft88@gmail.com>
2021-02-26 10:38:04 -0800

    switch statement

--- 136_HW1_hugoizquierdo.cpp
+++ 136_HW1_hugoizquierdo.cpp
@@ -5,6 +5,59 @@
 using namespace std;
 
 void romanType::printRom(char romNum(SIZE))
+{
+    cout << "The number in Roman numeral form is: " << romNum << endl;
+}
+
+int romanType::printDec(int sum)
+{
+    cout << "The number in decimal form is: " << sum << endl;
+    return sum;
+}
+
+int romanType::convertRom(char romNum(SIZE))
+{
+    int length = 0;
+    length = strlength(romNum);
+    int decTotal = 0;
+    int sum;
+
+    for (int count = 0; count < length; count++)
+    {
+        switch (romNum[count])
+        {
+            case 'M' : count = 1000;
+            break;
+
+            case 'D' : count = 500;
+            break;
+
+            case 'C' : count = 100;
+            break;
+
+            case 'L' : count = 50;
+            break;
+
+            case 'X' : count = 10;
+            break;
+
+            case 'V' : count = 5;
+            break;
+
+            case 'I' : count = 1;
+            break;
+
+            default: 
+            cout << "You did not enter a valid Roman Numeral";
+        }
+        dectotal = decTotal + count;
+    }
+    cout << decTotal << endl;
+    return decTotal;
+}
+
+
+
 
 int main()
 {
