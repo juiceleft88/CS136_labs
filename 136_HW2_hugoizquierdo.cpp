adade8f8a0209d91acc37eeeb2a4136f4d1ab755
juiceleft88 <juiceleft88@gmail.com>
2021-02-27 10:22:01 -0800

    commenting done

--- 136_HW2_hugoizquierdo.cpp
+++ 136_HW2_hugoizquierdo.cpp
@@ -4,57 +4,57 @@
 
 #include <iostream>
 #include <string>
-#include "136_HW2_hugoizquierdo.h"
+#include "136_HW2_hugoizquierdo.h" //including .h file
 
 using namespace std;
 
-dayType::dayType()
+dayType::dayType() //constructor
 {
     day = 0;
 }
 
-dayType::dayType(int d)
+dayType::dayType(int d) //constructor with integer parameter
 {
     day = d;
 }
 
-void dayType::setDay(int d)
+void dayType::setDay(int d) //sets integer value for day
 {
     day = d;
 }
 
-int dayType::getDay()
+int dayType::returnDay() //returns integer value for day
 {
     return day;
 }
 
 void dayType::printDay()
 {
-    cout << "The day is: " << dayName[day] << endl;
+    cout << "The day is: " << dayName[day] << endl; //prints string elements from array according to day that was set
 }
 
-std::string dayType::returnNext()
+std::string dayType::returnNext() //returns the next day in the form of string from array. 
 {
-    int otherDay = (day+1) % 7;
+    int otherDay = (day+1) % 7; // variable otherDay to manipulate array. Modulus used for giving proper day of week.
     return dayName[otherDay];
 }
 
-std::string dayType::returnPrev()
+std::string dayType::returnPrev() 
 {
-    int otherDay = (7 + (day-1)%7)%7;
+    int otherDay = (7 + (day-1)%7)%7; // variable otherDay used to manipulate array. Modulus used for giving proper day of week. Got this portion of code from the book, wasn't sure how to do the math properly for the 'previous day'.
     return dayName[otherDay];
 }
 
-std::string dayType::returnDayCalc(int d)
+std::string dayType::returnDayCalc(int d) //used if / else-if statement to calculate whether the days were positive (x days from now) or negative (previous days from the day)
 { int otherDay= 0;
-        if (d > 0)
+        if (d > 0) //if the day count is positive, means its in the future, means day addition will occur
     {
         otherDay = (day+d)%7;
         return dayName[otherDay];
     }
     else 
     {
-        int otherDay = (7 + (day+d)%7)%7;
+        int otherDay = (7 + (day+d)%7)%7; //but if day count is negative, means its in the past, means day subtraction will occur
         return dayName[otherDay];
     }
 
@@ -63,22 +63,22 @@ std::string dayType::returnDayCalc(int d)
 
 int main()
 {
+//invoking functions
+dayType firstDay; //creating an object of dayType
 
-dayType firstDay;
+firstDay.setDay(2); //change the argument from 2 to any other number to set the day
 
-firstDay.setDay(2);
+firstDay.printDay(); //prints the day
 
-firstDay.printDay();
-
-cout << "This is day number: " << firstDay.getDay() << endl;
+cout << "This is day number: " << firstDay.returnDay() << endl;
 
 cout << "The next day is: " << firstDay.returnNext() <<endl;
 
 cout << "The previous day was: " << firstDay.returnPrev() << endl;
 
-cout << "In 3 days, it will be: " << firstDay.returnDayCalc(3) << endl;
+cout << "In 3 days, it will be: " << firstDay.returnDayCalc(3) << endl; //positive number as an argument will add that many days into the future
 
-cout << "2 days ago, it was: " << firstDay.returnDayCalc(-2) <<endl;
+cout << "2 days ago, it was: " << firstDay.returnDayCalc(-2) <<endl; //negative number as an argument will subtract that many days from the current day.
 
 
 return 0;
--- 136_HW2_hugoizquierdo.h
+++ 136_HW2_hugoizquierdo.h
@@ -1,23 +1,22 @@
 //Hugo Izquierdo
 //class creation header file
 #pragma once
-
+ //class definition
 class dayType
 {
-    public:
+    public: //public members
 
-    dayType();
-    dayType(int d);
-    void setDay(int d);
-    int getDay();
-    void printDay();
-    int returnDay(int d);
-    std::string returnNext();
-    std::string returnPrev();
-    std::string returnDayCalc(int d);
+    dayType(); //constructor
+    dayType(int d); //constuctor with int parameter
+    void setDay(int d); // function to set the day, for the rest of functions to base off of. Does not print anything
+    void printDay(); // function to print the day of the week from string array
+    int returnDay(); // function to return the numerical day of the week (0-6, with 0 being Sunday)
+    std::string returnNext(); //function to return the next day of the week after setDay
+    std::string returnPrev(); //functio to return the previous day of the week before setDay
+    std::string returnDayCalc(int d); //function to return days prior and days before the setDay. 
 
 
-    private:
-    std::string dayName[7] = = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    int day;
+    private: //private members
+    std::string dayName[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; //string values for days of the week. Cannot figure out how to get warning message to not show up
+    int day; //day variable
 };
